b1234eb82506e32e6a0512cb308a59ebf705aa19
harshith <svs.harshith@gmail.com>
2020-01-23 19:48:03 +0530

    views and models updated

--- treasure/models.py
+++ treasure/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class player(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    current_level = models.IntegerField(default=1)
+    score = models.IntegerField(default=0)
+    rank = models.IntegerField(default=0)
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class level(models.Model):
+    l_number = models.IntegerField()
+    image = models.ImageField()
+    audio = models.FileField()
+    text = models.TextField()
+    answer = models.CharField(max_length=200)
+    numuser = models.IntegerField(default=0)
+    accuracy = models.FloatField(default=0)
+    wrong = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
+
+class total_level(models.Model):
+    totallevel = models.IntegerField(default=100)
+
+    def __str__(self):
+        return str(self.totallevel)
