bafcc1c1cadecc8a4ef8742209d0aabc73e85f4d
harshith <svs.harshith@gmail.com>
2020-01-23 23:06:52 +0530

    fixed static issue

--- oth/settings.py
+++ oth/settings.py
@@ -124,9 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"static"),
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
+
 
 AUTHENTICATION_BACKENDS = (
  'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
