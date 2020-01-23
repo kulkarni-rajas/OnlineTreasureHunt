e6f69e80b981135d520ed4eaa533545a35e617d9
Hsj_PC <himanshu.shekharjha.272@gmail.com>
2020-01-23 21:00:26 +0530

    fix bugs

--- oth/urls.py
+++ oth/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('treasure.urls')),
     path('',include('django.contrib.auth.urls')),
-    path('auth/',include('social_django.urls')),
+    path('auth/',include('social_django.urls',namespace='social')),
 ]
